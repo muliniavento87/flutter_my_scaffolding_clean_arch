fe6c30069c0e2cc8acb5ffd53935ac2ad7ffd74a
giuseppe.alfieri@spindox.it <giuseppe.alfieri@spindox.it>
2024-06-13 14:54:48 +0200

    fix .gitignore

---
+++ data_module/lib/env/config.dart
@@ -0,0 +1,6 @@
+/// [Config] is the abstract class describing the configurations data
+///
+/// Each implementation is based on the environment
+abstract class Config {
+  final String baseUrl = "";
+}
---
+++ data_module/lib/env/config_mock.dart
@@ -0,0 +1,10 @@
+import 'config.dart';
+
+/// [Config] is the abstract class describing the configurations data
+///
+/// Each implementation is based on the environment
+class ConfigMock implements Config {
+
+  @override
+  String get baseUrl => "https://google.com/"; //TODO
+}
